2930c4aa2dc9835ef2a3dd00e4d4444ba62ab332
vishali117 <vishalisaraj117@gmail.com>
2021-06-23 00:31:56 +0530

    monster-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component.jsx';
+
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+       monsters: [
+        {
+          name: 'Frankenstein',
+          id : 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id : 'asc2',
+        },
+        {
+          name: 'Zombie',
+          id : 'asc3'
+        },
+       ]
+    };
+  }
+
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json ())
+  .then(users => this.setState({ monsters: users }));
+}
+
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <CardList monsters = {this.state.monsters}>
+        
+      </CardList>
+
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './card.style.css';
+
+export const Card = (props)  => (
+
+      <div className = 'card-container'>
+  
+        <img alt ="monsters" src={`https://robohash.org/${props.monster.id}?set=set2&size=180*180`} />
+
+        <h1> {props.monster.name} </h1>
+  
+      </div>
+   
+);
